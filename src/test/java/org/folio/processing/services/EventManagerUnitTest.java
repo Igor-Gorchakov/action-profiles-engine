387c6e46b17154bdf66249f6493a12b41370278c
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2019-10-28 18:28:30 +0200

    Applied formatting changes for tests

--- src/test/java/org/folio/processing/services/EventManagerUnitTest.java
+++ src/test/java/org/folio/processing/services/EventManagerUnitTest.java
@@ -16,75 +16,75 @@ import java.util.Arrays;
 @RunWith(VertxUnitRunner.class)
 public class EventManagerUnitTest {
 
-  private EventHandler createInstanceEventHandler = new CreateInstanceEventHandler();
-  private EventHandler createHoldingsRecordEventHandler = new CreateHoldingsRecordEventHandler();
-  private EventHandler createItemRecordEventHandler = new CreateItemRecordEventHandler();
+    private EventHandler createInstanceEventHandler = new CreateInstanceEventHandler();
+    private EventHandler createHoldingsRecordEventHandler = new CreateHoldingsRecordEventHandler();
+    private EventHandler createItemRecordEventHandler = new CreateItemRecordEventHandler();
 
-  @Test
-  public void shouldHandleEvent(TestContext testContext) {
-    // given
-    EventManager eventManager = new EventManager();
-    eventManager.registerHandler(createInstanceEventHandler);
-    eventManager.registerHandler(createHoldingsRecordEventHandler);
-    eventManager.registerHandler(createItemRecordEventHandler);
+    @Test
+    public void shouldHandleEvent(TestContext testContext) {
+        // given
+        EventManager eventManager = new EventManager();
+        eventManager.registerHandler(createInstanceEventHandler);
+        eventManager.registerHandler(createHoldingsRecordEventHandler);
+        eventManager.registerHandler(createItemRecordEventHandler);
 
-    int expectedEventChainSize = 3;
+        int expectedEventChainSize = 3;
 
-    EventContext eventContext = new EventContext();
-    eventContext.setEventType("CREATED_SRS_MARC_BIB_RECORD");
+        EventContext eventContext = new EventContext();
+        eventContext.setEventType("CREATED_SRS_MARC_BIB_RECORD");
 
-    // when
-    eventManager.handleEvent(eventContext).setHandler(ar -> {
-      // then
-      testContext.assertTrue(ar.succeeded());
-      testContext.assertTrue(eventContext.isHandled());
-      testContext.assertEquals(expectedEventChainSize, eventContext.getEventChain().size());
-      testContext.assertEquals(
-        eventContext.getEventChain(),
-        Arrays.asList("CREATED_SRS_MARC_BIB_RECORD", "CREATED_INVENTORY_INSTANCE", "CREATED_HOLDINGS_RECORD")
-      );
-      testContext.assertEquals("CREATED_ITEM_RECORD", eventContext.getEventType());
-    });
-  }
+        // when
+        eventManager.handleEvent(eventContext).setHandler(ar -> {
+            // then
+            testContext.assertTrue(ar.succeeded());
+            testContext.assertTrue(eventContext.isHandled());
+            testContext.assertEquals(expectedEventChainSize, eventContext.getEventChain().size());
+            testContext.assertEquals(
+                    eventContext.getEventChain(),
+                    Arrays.asList("CREATED_SRS_MARC_BIB_RECORD", "CREATED_INVENTORY_INSTANCE", "CREATED_HOLDINGS_RECORD")
+            );
+            testContext.assertEquals("CREATED_ITEM_RECORD", eventContext.getEventType());
+        });
+    }
 
-  @Test
-  public void shouldNotHandleEventIfNoHandlersDefined(TestContext testContext) {
-    // given
-    EventManager eventManager = new EventManager();
-    int expectedEventChainSize = 0;
+    @Test
+    public void shouldNotHandleEventIfNoHandlersDefined(TestContext testContext) {
+        // given
+        EventManager eventManager = new EventManager();
+        int expectedEventChainSize = 0;
 
-    EventContext eventContext = new EventContext();
-    eventContext.setEventType("CREATED_SRS_MARC_BIB_RECORD");
+        EventContext eventContext = new EventContext();
+        eventContext.setEventType("CREATED_SRS_MARC_BIB_RECORD");
 
-    // when
-    eventManager.handleEvent(eventContext).setHandler(ar -> {
-      // then
-      testContext.assertTrue(ar.succeeded());
-      testContext.assertFalse(eventContext.isHandled());
-      testContext.assertEquals(expectedEventChainSize, eventContext.getEventChain().size());
-      testContext.assertEquals("CREATED_SRS_MARC_BIB_RECORD", eventContext.getEventType());
-    });
-  }
+        // when
+        eventManager.handleEvent(eventContext).setHandler(ar -> {
+            // then
+            testContext.assertTrue(ar.succeeded());
+            testContext.assertFalse(eventContext.isHandled());
+            testContext.assertEquals(expectedEventChainSize, eventContext.getEventChain().size());
+            testContext.assertEquals("CREATED_SRS_MARC_BIB_RECORD", eventContext.getEventType());
+        });
+    }
 
-  @Test
-  public void shouldNotHandleEventIfNoHandlersFound(TestContext testContext) {
-    EventManager eventManager = new EventManager();
-    eventManager.registerHandler(createInstanceEventHandler);
-    eventManager.registerHandler(createHoldingsRecordEventHandler);
-    eventManager.registerHandler(createItemRecordEventHandler);
+    @Test
+    public void shouldNotHandleEventIfNoHandlersFound(TestContext testContext) {
+        EventManager eventManager = new EventManager();
+        eventManager.registerHandler(createInstanceEventHandler);
+        eventManager.registerHandler(createHoldingsRecordEventHandler);
+        eventManager.registerHandler(createItemRecordEventHandler);
 
-    int expectedEventChainSize = 0;
+        int expectedEventChainSize = 0;
 
-    EventContext eventContext = new EventContext();
-    eventContext.setEventType("UNDEFINED_EVENT");
+        EventContext eventContext = new EventContext();
+        eventContext.setEventType("UNDEFINED_EVENT");
 
-    // when
-    eventManager.handleEvent(eventContext).setHandler(ar -> {
-      // then
-      testContext.assertTrue(ar.succeeded());
-      testContext.assertFalse(eventContext.isHandled());
-      testContext.assertEquals(expectedEventChainSize, eventContext.getEventChain().size());
-      testContext.assertEquals("UNDEFINED_EVENT", eventContext.getEventType());
-    });
-  }
+        // when
+        eventManager.handleEvent(eventContext).setHandler(ar -> {
+            // then
+            testContext.assertTrue(ar.succeeded());
+            testContext.assertFalse(eventContext.isHandled());
+            testContext.assertEquals(expectedEventChainSize, eventContext.getEventChain().size());
+            testContext.assertEquals("UNDEFINED_EVENT", eventContext.getEventType());
+        });
+    }
 }
